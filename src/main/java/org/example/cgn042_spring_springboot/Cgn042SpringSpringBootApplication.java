ee396ca8b2b8e2ff1d8a31021af32b478177b2a2
barbarahellmann <babshellmann@web.de>
2024-04-02 16:08:14 +0200

    Finished Challenge

--- src/main/java/org/example/cgn042_spring_springboot/Cgn042SpringSpringBootApplication.java
+++ src/main/java/org/example/cgn042_spring_springboot/Cgn042SpringSpringBootApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Cgn042SpringSpringBootApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Cgn042SpringSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Cgn042SpringSpringBootApplication.class, args);
+    }
 
 }
--- src/main/java/org/example/cgn042_spring_springboot/HelloController.java
+++ src/main/java/org/example/cgn042_spring_springboot/HelloController.java
@@ -1,2 +1,33 @@
-package org.example.cgn042_spring_springboot;public class HelloController {
+package org.example.cgn042_spring_springboot;
+
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+    // path variable
+    // http://localhost:8080/api/hello
+
+    @GetMapping("/hello")
+    public String sayHello(){
+      return "Hello, World!";
+    }
+
+    // path variable
+    // http://localhost:8080/api/hello/john
+
+    @GetMapping("/hello/{name}")
+    public String sayHelloJohn(@PathVariable String name){
+       return "Hello" + name;
+   }
+
+
+   //request param
+   // http://localhost:8080/api/greet?name=peter
+   @PostMapping("/greet")
+    public String greet(@RequestParam String name){
+        return "Hello " + name;
+    }
 }
